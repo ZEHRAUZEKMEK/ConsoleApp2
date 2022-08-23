b920920a6cd26e573fd3cfe528bd99b7af1e998a
zehra <zehra@DESKTOP-P48UVDQ>
2022-08-23 11:46:45 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int say1 = 10;
+            int say2 = 30;
+            say1 = say2;
+            say2 = 64;
+            //bu durumda say1=30
+
+            int[] sayilar1 = new int[] { 10, 20, 30 };
+            int[] sayilar2 = new int[] { 100, 200, 300 };
+            sayilar1 = sayilar2;
+            sayilar2[0] = 999;
+            //bu durumda sayilar1[0]=999
+        }
+    }
+}
